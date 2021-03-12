bf967504f0412e23eb8b2a7c3002ff4f31e34144
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-03-12 22:32:21 +0200

    btn

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(function(){
       infinite: true,
       speed: 1500,
       autoplay: true,
-      arrows: true
+      arrows: true,
+      prevArrow:'<button class="slider-btn slider-btnPrev"><img src="img/arrow-btnNext.svg">',
+nextArrow:'<button class="slider-btn slider-btnNext"><img src="img/arrow-btnPrev.svg">'
  
 });
 });
